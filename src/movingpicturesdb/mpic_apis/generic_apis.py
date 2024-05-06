0558c07c8da98f9d21e29fc8c64e0c889896251f
Giorgos <george.karalis@l3harris.com>
2024-05-06 22:15:00 +0100

    Connect endpoints to DB

--- src/movingpicturesdb/mpic_apis/generic_apis.py
+++ src/movingpicturesdb/mpic_apis/generic_apis.py
@@ -4,7 +4,6 @@ import psycopg
 from fastapi import Depends, FastAPI, HTTPException, Response, status
 from movingpicturesdb import database as DB
 from movingpicturesdb import models, schemas
-from movingpicturesdb.dummy_movies import movies
 from psycopg.rows import dict_row
 from sqlalchemy.orm import Session
 
@@ -34,35 +33,50 @@ while True:
 
 
 @mpic.get("/movies/all")
-async def get_all_movies(db: Session = Depends(DB.get_db)) -> dict:
+async def get_all_movies(db: Session = Depends(DB.get_db)):
     """Get all Moving Picture entries"""
     all_movies = db.query(models.MovingPicture).all()
-    return {"data": all_movies}
+    return all_movies
 
 
-@mpic.get("/movies/latest")
-async def get_latest_movie() -> dict:
+@mpic.get(
+    "/movies/latest",
+    response_model=schemas.MovingPictureResponse,
+)
+async def get_latest_movie(db: Session = Depends(DB.get_db)):
     """Get the most recently added movie."""
-    movie = movies[-1]
+    qs_movies = db.query(models.MovingPicture).order_by(models.MovingPicture.id.desc())
+    last_movie = qs_movies.first()
+
+    return last_movie
+
+
+@mpic.get(
+    "/movies/{id}",
+    response_model=schemas.MovingPictureResponse,
+)
+async def get_movie(id: int, db: Session = Depends(DB.get_db)):
+    """Get a single movie entry, using the movie's PK."""
+    qs_movies = db.query(models.MovingPicture).filter(models.MovingPicture.id == id)
+
+    movie = qs_movies.first()
+    if movie is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Retrieval Failed. Movie {id} was not found.",
+        )
     return movie
 
 
-@mpic.get("/movies/{id}")
-async def get_movie(id: int, db: Session = Depends(DB.get_db)) -> dict:
-    """Get a single movie entry, using the movie's PK."""
-    movie = db.query(models.MovingPicture).filter(models.MovingPicture.id == id).first()
-    if movie is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="No movie could be found."
-        )
-    return {"data": movie}
-
-
-@mpic.post("/new-movie", status_code=status.HTTP_201_CREATED)
-async def create_movie (
-    movie: schemas.MovingPicture,
+@mpic.post(
+    "/new-movie",
+    status_code=status.HTTP_201_CREATED,
+    response_model=schemas.MovingPictureResponse,
+)
+async def create_movie(
+    movie: schemas.CreateMovingPicture,
     db: Session = Depends(DB.get_db),
-) -> dict:
+):
     """Adds new entry in the the MovingPicture Table"""
     print(movie.model_dump())
     new_movie = models.MovingPicture(**movie.model_dump())
@@ -71,22 +85,44 @@ async def create_movie (
     db.commit()
     db.refresh(new_movie)
 
-    return {"data": new_movie}
+    return new_movie
 
 
-@mpic.update("/movies/{id}")
-async def update_movie(id:int, db: Session = Depends(DB.get_db)) -> dict:
-
-
-
-@mpic.delete("/movies/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_movie(id: int, db: Session = Depends(DB.get_db)) -> dict:
-    """Remove a single movie entry from the DB, using the movie's PK."""
-    movie = movie = db.query(models.MovingPicture).filter(models.MovingPicture.id == id)
-    if movie.first() is None:
+@mpic.put(
+    "/movies/{id}",
+    response_model=schemas.MovingPictureResponse,
+)
+async def update_movie(
+    id: int,
+    updated_movie: schemas.CreateMovingPicture,
+    db: Session = Depends(DB.get_db),
+):
+    qs_movies = db.query(models.MovingPicture).filter(models.MovingPicture.id == id)
+    if qs_movies.first() is None:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="No movie could be found."
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Update Failed. Movie {id} was not found.",
         )
-    movie.delete(synchronize_session=False)
+
+    qs_movies.update(updated_movie.model_dump(), synchronize_session=False)
+    db.commit()
+
+    return qs_movies.first()
+
+
+@mpic.delete(
+    "/movies/{id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete_movie(id: int, db: Session = Depends(DB.get_db)):
+    """Remove a single movie entry from the DB, using the movie's PK."""
+    qs_movies = db.query(models.MovingPicture).filter(models.MovingPicture.id == id)
+    mov_to_del = qs_movies.first()
+    if mov_to_del is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Deletion Failed. Movie {id} was not found.",
+        )
+    qs_movies.delete(synchronize_session=False)
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
